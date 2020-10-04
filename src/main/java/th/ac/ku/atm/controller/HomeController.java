a0933167602b5e7ad4b9438976fd56b4811623c9
numin123 <numin123@gmail.com>
2020-10-04 13:57:03 +0700

    add lab 8.3

--- src/main/java/th/ac/ku/atm/controller/HomeController.java
+++ src/main/java/th/ac/ku/atm/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model) {
-        model.addAttribute("greeting", "Sawadee");
+        model.addAttribute("greeting", "Sawadee Kub");
         return "home";//home.html
     }
 }
